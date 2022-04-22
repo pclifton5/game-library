95bed3a4fbb1b8e8e0394324430b1b6c24707162
pclifton <pclifton739@gmail.com>
2022-04-21 22:29:04 -0500

    Added fields

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+    //Fields for main here
+    Scanner scan = new Scanner(System.in);
+
+    //Library intro first
+    //Option to view library followed by view library list
+    //library inventory list method() here
+    //Option to edit library
+    //Add/Remove/Edit games method() here
+    //Option to exit Library
+    // Exit Library method() here
 }
